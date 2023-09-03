0bf5b9a060e7aa641fb26128f3229382dce88a42
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-03 20:58:31 +0100

    feat:(implementation of header and route setup)

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
-import { Button } from 'react-bootstrap';
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import {Header} from './component'
 
 const App = () => {
   return (
     <div>
-      <Button variant="primary">Primary</Button>{" "}
-      <Button variant="secondary">Secondary</Button>{" "}
-      <Button variant="success">Success</Button>{" "}
-      <Button variant="warning">Warning</Button>{" "}
-      <Button variant="danger">Danger</Button>{" "}
-      <Button variant="info">Info</Button>{" "}
-      <Button variant="light">Light</Button>{" "}
-      <Button variant="dark">Dark</Button>
-      <Button variant="link">Link</Button>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={'Home page'} />
+        </Routes>
+      </Router>
     </div>
   );
 }
