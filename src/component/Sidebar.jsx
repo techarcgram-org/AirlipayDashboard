a21e0bbe736d13b539d455d583f10d20512b023d
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-04 14:16:51 +0100

    feat:(side bar)

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,97 +1,38 @@
 import React from "react";
-import { IconContext } from "react-icons";
-// import {
-//   BsSearch,
-//   BsBell,
-//   FiSettings,
-// } from "react-icons";
-import { AiFillHome, AiOutlineMail, AiFillCopy } from "react-icons/ai";
-import {
-  BsFillAirplaneFill,
-  BsPersonCircle,
-  BsQuestionSquareFill,
-} from "react-icons/bs";
-import { Nav, Navbar } from "react-bootstrap";
 import classNames from "classnames";
+import { Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
-import SubMenu from "./Submenu";
+import data from "../constant/data";
 
 const SideBar = ({ isOpen, toggle }) => (
-  <div className={classNames("sidebar", { "is-open": isOpen })}>
+  <div
+    className={classNames("sidebar", { "is-open": isOpen })}
+    style={{ backgroundColor: "#785AE6",width:'30%',maxWidth:'250px',height:'100vh - 8rem' }}
+  >
     <div className="sidebar-header">
       <span color="info" onClick={toggle} style={{ color: "#fff" }}>
         &times;
       </span>
-      {/* <h3>Bootstrap Sidebar</h3> */}
     </div>
     <div className="side-menu">
       <Nav className="flex-column pb-3">
-        {/* <p>Dummy Heading</p> */}
-        {/* <SubMenu title="Home" icon={<AiFillHome />} items={submenus[0]} /> */}
-        <Nav.Item>
-          <Nav.Link as={Link} to={"/about"}>
-            <IconContext.Provider value={{ className: "mr-2" }}>
-              <AiOutlineMail />
-            </IconContext.Provider>
-            Dashboard
-          </Nav.Link>
-        </Nav.Item>
-        {/* <SubMenu title="Pages" icon={<AiFillCopy />} items={submenus[1]} /> */}
-        <Nav.Item>
-          <Nav.Link as={Link} to={"/pages"}>
-            <IconContext.Provider value={{ className: "mr-2" }}>
-              <BsPersonCircle />
-            </IconContext.Provider>
-            Clients
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link as={Link} to={"/faq"}>
-            <IconContext.Provider value={{ className: "mr-2" }}>
-              <BsQuestionSquareFill />
-            </IconContext.Provider>
-            Admins
-          </Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link as={Link} to={"/contact"}>
-            <IconContext.Provider value={{ className: "mr-2" }}>
-              <BsFillAirplaneFill />
-            </IconContext.Provider>
-            Users
-          </Nav.Link>
-        </Nav.Item>
+        {data.sidebarData.map((item) => (
+          <Nav.Item key={item.route}>
+            <Nav.Link style={{color:'#fff',marginBottom:'1rem'}} as={Link} to={item.route}>
+              <img
+                src={item.image}
+                alt=""
+                width={25}
+                style={{ marginRight: 12 }}
+              />
+              {item.name}
+            </Nav.Link>
+          </Nav.Item>
+        ))}
       </Nav>
     </div>
   </div>
 );
-
-const submenus = [
-  [
-    {
-      title: "Home 1",
-      target: "Home-1",
-    },
-    {
-      title: "Home 2",
-      target: "Home-2",
-    },
-    {
-      title: "Home 3",
-      target: "Home-3",
-    },
-  ],
-  [
-    {
-      title: "Page 1",
-      target: "Page-1",
-    },
-    {
-      title: "Page 2",
-      target: "Page-2",
-    },
-  ],
-];
 
 export default SideBar;
---
+++ src/constant/images.js
@@ -0,0 +1,24 @@
+
+import bank from '../assets/images/banks.svg'
+import processing from '../assets/images/built in payment processing.svg'
+import address from '../assets/images/business address.svg'
+import client from '../assets/images/client.jpg'
+import group from '../assets/images/Group.jpg'
+import home from '../assets/images/icon _home_.jpg'
+import payment from '../assets/images/payment.svg'
+import person from '../assets/images/person.jpg'
+import withdraw from '../assets/images/withdraw.svg'
+
+const images = {
+  bank,
+  processing,
+  address,
+  client,
+  group,
+  home,
+  payment,
+  person,
+  withdraw
+}
+
+export default images
