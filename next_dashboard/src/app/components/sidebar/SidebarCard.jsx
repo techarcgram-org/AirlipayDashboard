fd2179a93603f022c5c21025867f68791a22dbe8
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-26 00:40:29 +0100

    rewriting the sidebar

--- next_dashboard/src/app/components/sidebar/SidebarCard.jsx
+++ next_dashboard/src/app/components/sidebar/SidebarCard.jsx
@@ -1,23 +1,30 @@
+import images from '@/app/constant/images';
+import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import React from 'react'
 
-const SidebarCard = ({to,text,icon}) => {
+const SidebarCard = ({route,name,icon}) => {
+  const router = useRouter();
   return (
     <div>
-      <Link href={to}>
+      <Link href={`${route}`}>
         <div
-          className={`pl-6 py-3 mx-5 rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
+          className={`-pl-4 py-3 mx-2 relative rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
             router.pathname == "/billing"
               ? "bg-orange-100 text-orange-500"
               : "text-gray-400 hover:bg-orange-100 hover:text-orange-500"
           }`}
         >
           <div className="mr-2">
-            {icon}
             {/* <CreditCardIcon className="h-5 w-5" /> */}
+            <Image width={40} height={40} src={icon} />
           </div>
           <div>
-            <p>{text}</p>
+            <p className="font-inter text-steelblue text-left">{name}</p>
+          </div>
+          <div className="absolute right-2 -ml-4">
+            <Image width={10} height={10} src={images.right} />
           </div>
         </div>
       </Link>
