d4f445e5a69773fdcea7c470882fbf04bc54a72d
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-09-27 12:42:18 +0100

    adding a page

---
+++ next_dashboard/src/app/users/page.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex flex-1 bg-red-500 w-full' >
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum neque, suscipit et mollitia pariatur amet nihil quibusdam temporibus accusantium quo hic doloremque eius, ad laudantium at molestias facilis ipsam quas magni. Id, ducimus maiores? Deserunt molestias optio, dolorem officiis, cumque qui vero excepturi ad maxime odit facilis saepe, eum laborum asperiores minus unde in. Quaerat non aperiam architecto sequi voluptatem.
+      </h1>
+    </div>
+  )
+}
+
+export default page
