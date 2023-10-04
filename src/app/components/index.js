c440e30868a0bd612e0bb3e20c62e77a5319492f
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-04 17:13:55 +0100

    created a tab reusable component

--- src/app/components/index.js
+++ src/app/components/index.js
@@ -4,3 +4,4 @@ export { default as Sidebar } from "./sidebar/Sidebar"
 export {default as SidebarCard} from './sidebar/SidebarCard'
 export {default as Table} from './user/Table'
 export {default as AddUser} from './user/AddUser'
+export {default as Tab} from './common/Tab'
--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,19 +1,19 @@
 'use client'
 
 import React, { useState } from "react";
-import { AddUser, Table } from "../components";
+import { AddUser, Tab, Table } from "../components";
 import data from "../constant/data";
 
 const Page = () => {
-  const [activeTab, setActiveTab] = useState("Tab1");
+  // const [activeTab, setActiveTab] = useState("Tab1");
 
-  const handleTabClick = (tab) => {
-    setActiveTab(tab);
-  };
+  // const handleTabClick = (tab) => {
+  //   setActiveTab(tab);
+  // };
 
   return (
     <div>
-      <div className="flex justify-center w-[400px] mb-4">
+      {/* <div className="flex justify-center w-[400px] mb-4">
         <button
           className={`px-4 py-2 ${
             activeTab === "Tab1" ? "bg-[#3F5F90] text-black" : "bg-gray-300"
@@ -51,7 +51,16 @@ const Page = () => {
         <h1>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore totam nisi reprehenderit aliquid illo possimus eaque, numquam sunt veritatis voluptatum eius.
         </h1>
-      )}
+      )} */}
+      <Tab options={["View User", "Add Users", "Edit Info"]} defaultTab="View User">
+        <Table tab="View User" users={data.usersTable} columns={data.columns} />
+        <AddUser tab="Add Users" />
+        <h1 tab="Edit Info">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+          totam nisi reprehenderit aliquid illo possimus eaque, numquam sunt
+          veritatis voluptatum eius.
+        </h1>
+      </Tab>
     </div>
   );
 };
