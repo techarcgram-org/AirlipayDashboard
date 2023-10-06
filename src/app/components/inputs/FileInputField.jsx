81a2649275ee3aad6af9c6a9cb7f6c026d702cb0
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-06 05:14:38 +0100

    created reusable components for the inputs

---
+++ src/app/components/inputs/FileInputField.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const FileInput = ({ label, name, onChange }) => {
+  return (
+    <div className="mb-4">
+      <label
+        htmlFor={name}
+        className="block font-medium mb-1"
+        style={{ color: "black" }}
+      >
+        {label}
+      </label>
+      <input
+        type="file"
+        id={name}
+        name={name}
+        accept="image/*"
+        onChange={onChange}
+        className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400"
+      />
+    </div>
+  );
+};
+
+export default FileInput;
