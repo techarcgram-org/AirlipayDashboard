49a04d477eba4b57d2bbf578efaa1cc11c8f1881
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-10-17 23:13:00 +0100

    changed the sidebar from the ugly images beautiful icons

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,9 +1,17 @@
 "use client";
 
-import data from "../../constant/data";
+import images from "@/constant/images";
+import Image from "next/image";
+import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { forwardRef } from "react";
-import { SidebarCard } from "..";
+import { AiOutlineHome } from "react-icons/ai";
+import { GiPayMoney, GiReceiveMoney } from "react-icons/gi";
+import { GrUserAdmin } from "react-icons/gr";
+import { HiUser, HiUsers } from "react-icons/hi";
+import {FcMoneyTransfer} from "react-icons/fc"
+import {BsBank} from "react-icons/bs"
+import {FaLocationPinLock} from "react-icons/fa"
 
 const SideBar = forwardRef(({ showNav }, ref) => {
   const router = useRouter();
@@ -17,9 +25,232 @@ const SideBar = forwardRef(({ showNav }, ref) => {
         <h2 className="text-center p-4 text-3xl text-white font-bold">
           Airlipay
         </h2>
-        {data.sidebarData.map((item) => (
+        {/* {data.sidebarData.map((item) => (
           <SidebarCard name={item.name} icon={item.image} route={item.route} />
-        ))}
+        ))} */}
+        <div>
+          <Link href="/">
+            <div
+              className={`-pl-4 py-3 mx-2 relative rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
+                router.pathname == "/billing"
+                  ? "bg-orange-100 text-orange-500"
+                  : "text-gray-400 hover:bg-orange-100 hover:text-orange-500"
+              }`}
+            >
+              <div className="flex flex-row items-center justify-around mr-2">
+                <div className="mr-2">
+                  <AiOutlineHome className="h-5 w-5" />
+                </div>
+                <div>
+                  <p className="font-inter text-steelblue text-left">
+                    Dashboard
+                  </p>
+                </div>
+              </div>
+              <div className="absolute right-3 -ml-4">
+                <Image width={10} height={10} src={images.right} />
+              </div>
+            </div>
+          </Link>
+        </div>
+
+        <div>
+          <Link href="/clients">
+            <div
+              className={`-pl-4 py-3 mx-2 relative rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
+                router.pathname == "/billing"
+                  ? "bg-orange-100 text-orange-500"
+                  : "text-gray-400 hover:bg-orange-100 hover:text-orange-500"
+              }`}
+            >
+              <div className="flex flex-row items-center justify-around mr-2">
+                <div className="mr-2">
+                  <HiUsers className="h-5 w-5" />
+                </div>
+                <div>
+                  <p className="font-inter text-steelblue text-left">Clients</p>
+                </div>
+              </div>
+              <div className="absolute right-3 -ml-4">
+                <Image width={10} height={10} src={images.right} />
+              </div>
+            </div>
+          </Link>
+        </div>
+
+        <div>
+          <Link href="/admins">
+            <div
+              className={`-pl-4 py-3 mx-2 relative rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
+                router.pathname == "/billing"
+                  ? "bg-orange-100 text-orange-500"
+                  : "text-gray-400 hover:bg-orange-100 hover:text-orange-500"
+              }`}
+            >
+              <div className="flex flex-row items-center justify-around mr-2">
+                <div className="mr-2">
+                  <GrUserAdmin className="h-5 w-5" />
+                </div>
+                <div>
+                  <p className="font-inter text-steelblue text-left">Admins</p>
+                </div>
+              </div>
+              <div className="absolute right-3 -ml-4">
+                <Image width={10} height={10} src={images.right} />
+              </div>
+            </div>
+          </Link>
+        </div>
+
+        <div>
+          <Link href="/users">
+            <div
+              className={`-pl-4 py-3 mx-2 relative rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
+                router.pathname == "/billing"
+                  ? "bg-orange-100 text-orange-500"
+                  : "text-gray-400 hover:bg-orange-100 hover:text-orange-500"
+              }`}
+            >
+              <div className="flex flex-row items-center justify-around mr-2">
+                <div className="mr-2">
+                  <HiUser className="h-5 w-5" />
+                </div>
+                <div>
+                  <p className="font-inter text-steelblue text-left">Users</p>
+                </div>
+              </div>
+              <div className="absolute right-3 -ml-4">
+                <Image width={10} height={10} src={images.right} />
+              </div>
+            </div>
+          </Link>
+        </div>
+
+        <div>
+          <Link href="/users/:id/withdraw">
+            <div
+              className={`-pl-4 py-3 mx-2 relative rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
+                router.pathname == "/billing"
+                  ? "bg-orange-100 text-orange-500"
+                  : "text-gray-400 hover:bg-orange-100 hover:text-orange-500"
+              }`}
+            >
+              <div className="flex flex-row items-center justify-around mr-2">
+                <div className="mr-2">
+                  <GiReceiveMoney className="h-5 w-5" />
+                </div>
+                <div>
+                  <p className="font-inter text-steelblue text-left">
+                    User Withdrawals
+                  </p>
+                </div>
+              </div>
+              <div className="absolute right-3 -ml-4">
+                <Image width={10} height={10} src={images.right} />
+              </div>
+            </div>
+          </Link>
+        </div>
+
+        <div>
+          <Link href="/ailipay">
+            <div
+              className={`-pl-4 py-3 mx-2 relative rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
+                router.pathname == "/billing"
+                  ? "bg-orange-100 text-orange-500"
+                  : "text-gray-400 hover:bg-orange-100 hover:text-orange-500"
+              }`}
+            >
+              <div className="flex flex-row items-center justify-around mr-2">
+                <div className="mr-2">
+                  <FcMoneyTransfer className="h-5 w-5" />
+                </div>
+                <div>
+                  <p className="font-inter text-steelblue text-left">
+                    User Airlipays
+                  </p>
+                </div>
+              </div>
+              <div className="absolute right-3 -ml-4">
+                <Image width={10} height={10} src={images.right} />
+              </div>
+            </div>
+          </Link>
+        </div>
+
+        <div>
+          <Link href="/clients/payment">
+            <div
+              className={`-pl-4 py-3 mx-2 relative rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
+                router.pathname == "/billing"
+                  ? "bg-orange-100 text-orange-500"
+                  : "text-gray-400 hover:bg-orange-100 hover:text-orange-500"
+              }`}
+            >
+              <div className="flex flex-row items-center justify-around mr-2">
+                <div className="mr-2">
+                  <GiPayMoney className="h-5 w-5" />
+                </div>
+                <div>
+                  <p className="font-inter text-steelblue text-left">
+                    Clients Payment
+                  </p>
+                </div>
+              </div>
+              <div className="absolute right-3 -ml-4">
+                <Image width={10} height={10} src={images.right} />
+              </div>
+            </div>
+          </Link>
+        </div>
+
+        <div>
+          <Link href="/banks">
+            <div
+              className={`-pl-4 py-3 mx-2 relative rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
+                router.pathname == "/billing"
+                  ? "bg-orange-100 text-orange-500"
+                  : "text-gray-400 hover:bg-orange-100 hover:text-orange-500"
+              }`}
+            >
+              <div className="flex flex-row items-center justify-around mr-2">
+                <div className="mr-2">
+                  <BsBank className="h-5 w-5" />
+                </div>
+                <div>
+                  <p className="font-inter text-steelblue text-left">Banks</p>
+                </div>
+              </div>
+              <div className="absolute right-3 -ml-4">
+                <Image width={10} height={10} src={images.right} />
+              </div>
+            </div>
+          </Link>
+        </div>
+
+        <div>
+          <Link href="/address">
+            <div
+              className={`-pl-4 py-3 mx-2 relative rounded text-center cursor-pointer mb-3 flex items-center transition-colors ${
+                router.pathname == "/billing"
+                  ? "bg-orange-100 text-orange-500"
+                  : "text-gray-400 hover:bg-orange-100 hover:text-orange-500"
+              }`}
+            >
+              <div className="flex flex-row items-center justify-around mr-2">
+                <div className="mr-2">
+                  <BsBank className="h-5 w-5" />
+                </div>
+                <div>
+                  <p className="font-inter text-steelblue text-left">Address</p>
+                </div>
+              </div>
+              <div className="absolute right-3 -ml-4">
+                <Image width={10} height={10} src={images.right} />
+              </div>
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
