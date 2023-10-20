6dfc0fb17fcb3d8960d93450fd4c9bd2a0e47139
Sheyrhon <chisheyrhon96@gmail.com>
2023-10-20 15:56:45 -0400

    adjusted spacing between fields

--- src/app/components/user/UserDetails.jsx
+++ src/app/components/user/UserDetails.jsx
@@ -1,18 +1,25 @@
 
-import React from 'react'
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEdit } from '@fortawesome/free-solid-svg-icons';
 import { Tab } from '..'
 
 const UserDetails = () => {
   return (
     <div className="flex itstrongs-center h-[300px] justify-around">
-      <div style={{textAlign:'start',paddingBottom:74,}}>User details
+      
+      <div style={{textAlign:'start',paddingBottom:74,}}>
+    <h2 style={{textAlign:'left', marginBottom:15}}>User details</h2>
+        
         <div className='flex'>
-          <p className='m-1'>  <strong>Alvin Ana  </strong> #9753428</p> 
+          <p className='m-2'>  <strong>Alvin Ana  </strong> #9753428</p> 
           <button className='m-1'style={{backgroundColor:'green',padding:6,color:'white', borderRadius:3}}>Active</button>
-          <p className='m-1 text-blue'>edit</p>
+          <FontAwesomeIcon icon={faEdit} />
+          <p className='m-2'>edit</p>
+          
         </div>
         <div className='flex'>
-            <div className='m-2'>
+            <div className='m-3'>
                 <ol>
                   <li>Email:</li>
                   <li>Tel:</li>
@@ -22,8 +29,8 @@ const UserDetails = () => {
                 </ol>
 
             </div>
-            <div className='space h-2 w-2'></div>
-            <div className='m-2'>
+            <div className='space h-1 w-2'></div>
+            <div className='m-3'>
             <ol>
                   <li>mail@gmail.com</li>
                   <li>+237 999 999 999</li>
@@ -34,35 +41,30 @@ const UserDetails = () => {
             </div>
         </div>
        
-        {/* <div className='h-[90%] w-[2px] bg-black' ></div> */}
-
-        {/* <div>
-          
-        </div> */}
-      </div>
+        </div>
       <div className='h-[90%] w-[2px] bg-black' ></div>
       {/* the code below is for the balance section */}
-      <div><strong>BALANCE DETAILS</strong>
+      <div style={{textAlign:'center', marginBottom:3}}><strong>BALANCE DETAILS</strong>
       <div className='flex text'>
-          <div className='m-2'>
-              <p className='text-blue'><strong>Available Banlance</strong></p>
-              <p>XAF 5000</p>
+          <div className='m-10'>
+              <p className='text-blue'>Available Banlance</p>
+              <p style={{color:'#063B87', fontSize:20}}>XAF 5000</p>
             </div>
-            <div className='m-2'>
-            <p><strong>Transferred </strong></p>
-              <p>XAF 5000</p>
+            <div className='m-10'>
+            <p style={{fontSize:15}}>Transferred </p>
+            <p style={{color:'#063B87', fontSize:20}}>XAF 5000</p>
             </div>
       </div>
         
         {/* <div className='h-[90%] w-[2px] bg-black' ></div> */}
         <div className='flex'>
-        <div className='m-2'>
-        <p> <strong>Negative Banlance</strong></p>
-          <p>XAF 5000</p>
+        <div className='m-10'>
+        <p > Negative Banlance</p>
+        <p style={{color:'#063B87', fontSize:20}}>XAF 5000</p>
         </div>
-        <div className='m-2'>
-        <p><strong>Fees</strong></p>
-          <p>XAF 5000</p>
+        <div className='m-10'>
+        <p>Fees</p>
+        <p style={{color:'#063B87', fontSize:20}}>XAF 5000</p>
         </div>
 
         </div>
