07ab948cd088a7317b9d04803bdcb726f2537995
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-11-15 14:23:16 +0100

    removing commented codes | review

--- src/app/clients/page.jsx
+++ src/app/clients/page.jsx
@@ -12,22 +12,13 @@ import Link from "next/link";
 const Page = () => {
   console.log(transactions)
   return (
-    // <div>Clients
-    //   <Table users={data.clientsTable} columns={data.clientColumns} />
-    // </div>
     <div className="mt-1 md:right-6" ><h2>Clients</h2>
-    {/* <Login/> */}
     <Tab
       options={["View Clients", "Add Clients"]}
       defaultTab="View Clients"
     >
       <Table tab="View Clients" users={data.usersTable} columns={data.columns} />
       <AddUser tab="Add Clients" />
-      {/* <h1 tab="Edit Info">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
-        totam nisi reprehenderit aliquid illo possimus eaque, numquam sunt
-        veritatis voluptatum eius.
-      </h1> */}
     </Tab>
   </div>
     );
--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -5,11 +5,6 @@ import { AddUser, Tab, Table, UserDetails } from "../../components";
 import data from "../../constant/data";
 
 const Page = () => {
-  // const [activeTab, setActiveTab] = useState("Tab1");
-
-  // const handleTabClick = (tab) => {
-  //   setActiveTab(tab);
-  // };
 
   return (
     <div className="mt-1 md:right-6" > <h2 className="mb-5">Users</h2>
@@ -19,11 +14,6 @@ const Page = () => {
       >
         <Table tab="View Users" users={data.usersTable} columns={data.columns} />
         <AddUser tab="Add Users" />
-        {/* <h1 tab="Edit Info">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
-          totam nisi reprehenderit aliquid illo possimus eaque, numquam sunt
-          veritatis voluptatum eius.
-        </h1> */}
       </Tab>
     </div>
   );
