e6aff54cb336a270d821546bd853bb801233bd6d
TabotCharlesBessong <ebezebeatrice@gmail.com>
2023-11-17 07:47:15 +0100

    the form for the addtion of users

--- src/components/userWithdrawals/AddUser.jsx
+++ src/components/userWithdrawals/AddUser.jsx
@@ -3,15 +3,16 @@ import { CheckboxInput, FileInput, TextInput } from "..";
 
 const AddUserForm = () => {
   const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    profession: "",
+    fullName: "",
+    phoneNumber: "",
+    street: "",
     email: "",
     employer: "",
-    idCard: null,
+    secondPhoneNumber: "",
     taxPayerNumber: "",
-    isMarried: false,
-    hasChildren: false,
+    dateOfBirth: "",
+    city: "",
+    baseSalary:""
   });
 
   const handleInputChange = (event) => {
@@ -52,31 +53,43 @@ const AddUserForm = () => {
 
   return (
     <div className="max-w-3xl mx-auto px-4">
+      {/* <CheckboxInput
+        label="Married"
+        name="isMarried"
+        checked={formData.isMarried}
+        onChange={handleInputChange}
+      />
+      <CheckboxInput
+        label="Has Children"
+        name="hasChildren"
+        checked={formData.hasChildren}
+        onChange={handleInputChange}
+      /> */}
       <form onSubmit={handleSubmit} className="flex justify-between">
         {/* First Column */}
         <div className="w-1/2 mr-2">
           <TextInput
-            label="First Name"
-            name="firstName"
-            value={formData.firstName}
+            label="Full Name"
+            name="fullName"
+            value={formData.fullName}
             onChange={handleInputChange}
-            placeholder="Enter your first name"
+            placeholder="Enter your full name"
             type="text"
           />
           <TextInput
-            label="Profession"
-            name="profession"
-            value={formData.profession}
+            label="Phone Number*"
+            name="phoneNumber"
+            value={formData.phoneNumber}
             onChange={handleInputChange}
-            placeholder="Enter your Profession"
+            placeholder="674561823"
             type="text"
           />
           <TextInput
-            label="Employer"
+            label="Street"
             name="employer"
-            value={formData.employer}
+            value={formData.street}
             onChange={handleInputChange}
-            placeholder="Enter your employer"
+            placeholder="Enter your street"
             type="text"
           />
           <TextInput
@@ -87,18 +100,18 @@ const AddUserForm = () => {
             placeholder="Enter your tax payer number"
             type="text"
           />
+          <TextInput
+            label="Date of birth"
+            name="dob"
+            value={formData.taxPayerNumber}
+            onChange={handleInputChange}
+            placeholder="Enter your date of birth"
+            type="text"
+          />
         </div>
 
         {/* Second Column */}
         <div className="w-1/2 ml-2">
-          <TextInput
-            label="Last Name"
-            name="lastName"
-            value={formData.lastName}
-            onChange={handleInputChange}
-            placeholder="Enter your last name"
-            type="text"
-          />
           <TextInput
             label="Email"
             name="email"
@@ -107,29 +120,55 @@ const AddUserForm = () => {
             placeholder="Enter your email address"
             type="email"
           />
-          <FileInput
+          <TextInput
+            label="Secondary phone number"
+            name="secondNumber"
+            value={formData.secondPhoneNumber}
+            onChange={handleInputChange}
+            placeholder="Secondary phone number"
+            type="text"
+          />
+          <TextInput
+            label="City*"
+            name="city"
+            value={formData.city}
+            onChange={handleInputChange}
+            placeholder="Enter your city"
+            type="text"
+          />
+          <TextInput
+            label="Base Salary"
+            name="baseSalary"
+            value={formData.baseSalary}
+            onChange={handleInputChange}
+            placeholder="356000 / month"
+            type="text"
+          />
+          <TextInput
+            label="Employer"
+            name="employer"
+            value={formData.employer}
+            onChange={handleInputChange}
+            placeholder="356000 / month"
+            type="text"
+          />
+          {/* <FileInput
             label="ID Card (Image)"
             name="idCard"
             onChange={handleFileInputChange}
-          />
-          <CheckboxInput
-            label="Married"
-            name="isMarried"
-            checked={formData.isMarried}
-            onChange={handleInputChange}
-          />
-          <CheckboxInput
-            label="Has Children"
-            name="hasChildren"
-            checked={formData.hasChildren}
-            onChange={handleInputChange}
-          />
+          /> */}
         </div>
 
         {/* <div>
           <button className="px-2 p-1 rounded-md bg-blue-500 capitalize text-white">add user</button>
         </div> */}
       </form>
+      <div
+        className="w-full bg-[#3F5F90] py-2 mt-8 capitalize text-white text-center text-xl cursor-pointer"
+        onClick={() => {}}
+      >
+        add users
+      </div>
     </div>
   );
 };
