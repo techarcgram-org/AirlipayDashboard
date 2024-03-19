7cb6b9e8f6daeefb4fe490833294a9661b4fb43f
spycode1 <tafodinho@gmail.com>
2024-03-19 14:02:30 +0100

    Adds docker compose changes

--- src/app/apiServices/axiosConfig.js
+++ src/app/apiServices/axiosConfig.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-console.log("base URL", process.env.NEXT_PUBLIC_BASE_URL);
-axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 export default axios;
