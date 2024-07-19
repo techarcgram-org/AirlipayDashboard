d0672aa38ebc857fc450cfb77af47649d68cdf30
Nde Lucien <chelucien08@gmail.com>
2024-07-19 15:05:41 +0100

    add fallback for default url

--- src/app/apiServices/authService.js
+++ src/app/apiServices/authService.js
@@ -1,5 +1,6 @@
-import axios from "./axiosConfig";
+import axios from './axiosConfig';
 
 export async function login(data) {
+  console.log(axios);
   return await axios.post(`auth/login`, data);
 }
--- src/app/apiServices/axiosConfig.js
+++ src/app/apiServices/axiosConfig.js
@@ -1,11 +1,15 @@
-import axios from "axios";
+import axios from 'axios';
 
-console.log("base URL", process.env.NEXT_PUBLIC_BASE_URL);
-axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL;
+console.log(
+  'base URL',
+  process.env.NEXT_PUBLIC_BASE_URL || 'http://195.154.118.26:3000'
+);
+axios.defaults.baseURL =
+  process.env.NEXT_PUBLIC_BASE_URL || 'http://195.154.118.26:3000';
 
-if (typeof window !== "undefined" && window.localStorage) {
-  const token = localStorage.getItem("token");
-  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+if (typeof window !== 'undefined' && window.localStorage) {
+  const token = localStorage.getItem('token');
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 }
 
 export default axios;
