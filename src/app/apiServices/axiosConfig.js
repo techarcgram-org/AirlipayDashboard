f2363c04558641980a49c6c008525e933a15aac9
enowdivine <sirdivine16@gmail.com>
2024-07-24 14:07:01 +0100

    api ednpoint updates

--- src/app/apiServices/axiosConfig.js
+++ src/app/apiServices/axiosConfig.js
@@ -2,10 +2,10 @@ import axios from 'axios';
 
 console.log(
   'base URL',
-  process.env.NEXT_PUBLIC_BASE_URL || 'http://195.154.118.26:3000'
+  process.env.NEXT_PUBLIC_BASE_URL || 'https://api.airlipay.com'
 );
 axios.defaults.baseURL =
-  process.env.NEXT_PUBLIC_BASE_URL || 'http://195.154.118.26:3000';
+  process.env.NEXT_PUBLIC_BASE_URL || 'https://api.airlipay.com';
 
 if (typeof window !== 'undefined' && window.localStorage) {
   const token = localStorage.getItem('token');
