4c04c25595ac751cdf267c6ea6555f2b7b1f1ccd
enowdivine <sirdivine16@gmail.com>
2024-08-09 15:26:49 +0100

    updates

--- src/components/userWithdrawals/Table.jsx
+++ src/components/userWithdrawals/Table.jsx
@@ -106,19 +106,19 @@ const Table = ({
       const data = { id: status.id, accountStatus: status.status };
       if (currentUrlUser === "clients") {
         dispatch(updateClientById(data));
-        window.location.reload();
+        // window.location.reload();
       }
       if (currentUrlUser === "admins") {
         dispatch(updateAdmin(data));
-        window.location.reload();
+        // window.location.reload();
       }
       if (currentUrlUser === "users") {
         dispatch(updateUser(data));
-        window.location.reload();
+        // window.location.reload();
       }
       if (currentUrlUser === "payments") {
         dispatch(updateInvoice(data));
-        window.location.reload();
+        // window.location.reload();
       }
       if (currentUrlUser === "invoices") {
         const invoiceData = { id: status.id, status: status.status };
