679d7517a0ec2cbe6a148e3df456d17bb002034f
Nde Lucien <chelucien08@gmail.com>
2024-08-09 16:23:36 +0100

    you can take it from here

--- src/app/apiServices/axiosConfig.js
+++ src/app/apiServices/axiosConfig.js
@@ -1,11 +1,8 @@
 import axios from 'axios';
-
-console.log(
-  'base URL',
-  process.env.NEXT_PUBLIC_BASE_URL || 'https://api.airlipay.com'
-);
-axios.defaults.baseURL =
-  process.env.NEXT_PUBLIC_BASE_URL || 'https://api.airlipay.com';
+const baseURL = 'https://api.airlipay.com';
+// const baseURL = 'http://localhost:8004';
+console.log('base URL', process.env.NEXT_PUBLIC_BASE_URL || baseURL);
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL || baseURL;
 
 if (typeof window !== 'undefined' && window.localStorage) {
   const token = localStorage.getItem('token');
--- src/app/apiServices/invoiceService.js
+++ src/app/apiServices/invoiceService.js
@@ -1,36 +1,47 @@
-import axios from "./axiosConfig";
+import axios from './axiosConfig';
 
 export async function getInvoices() {
-  const token = localStorage.getItem("token");
-  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  const token = localStorage.getItem('token');
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
   return await axios.get(`/invoice`);
 }
 
 export async function getInvoiceTransactions(id) {
-  const token = localStorage.getItem("token");
-  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  const token = localStorage.getItem('token');
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
   return await axios.get(`/invoice/${id}/transactions`);
 }
 
 export async function editInvoice(data) {
-  const token = localStorage.getItem("token");
-  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-  return await axios.patch(`/invoice/${data.id}/update-status`, data, {
-    headers: {
-      "Content-Type": 'application/json'
+  const token = localStorage.getItem('token');
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  const res = await axios.put(
+    `/invoice/${data.id}/update-status`,
+    { status: data.accountStatus },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
     }
-  });
+  );
+
+  return res;
 }
 
 export async function markInvoiceAsComplete(data) {
-  const token = localStorage.getItem("token");
-  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-  console.log(data)
-  return await axios.patch(`/client/${data.id}/update-invoice-status`, data, {
-    headers: {
-      "Content-Type": 'application/json'
+  const token = localStorage.getItem('token');
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  console.log(data);
+  console.log(`/client/invoice/${data.id}/update-invoice-status`);
+  return await axios.put(
+    `/client/${data.id}/update-invoice-status`,
+    {
+      status: data.status,
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
     }
-  });
+  );
 }
-
-
