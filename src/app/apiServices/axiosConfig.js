6360a6162cb9cb15e26b9f74c69ac985672933d4
Nde Lucien <chelucien08@gmail.com>
2024-08-12 12:01:09 +0100

    fix update enpoint

--- src/app/apiServices/axiosConfig.js
+++ src/app/apiServices/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const baseURL = 'https://api.airlipay.com';
-// const baseURL = 'http://localhost:8004';
+// const baseURL = 'https://api.airlipay.com';
+const baseURL = 'http://localhost:8004';
 console.log('base URL', process.env.NEXT_PUBLIC_BASE_URL || baseURL);
 axios.defaults.baseURL = process.env.NEXT_PUBLIC_BASE_URL || baseURL;
 
--- src/app/apiServices/clientService.js
+++ src/app/apiServices/clientService.js
@@ -1,9 +1,9 @@
-import axios from "./axiosConfig";
+import axios from './axiosConfig';
 
 export async function createClient(data) {
-  const token = localStorage.getItem("token");
-  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-  axios.defaults.headers["Content-Type"] = "multipart/form-data";
+  const token = localStorage.getItem('token');
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  axios.defaults.headers['Content-Type'] = 'multipart/form-data';
   return await axios.post(`/client`, data, {
     headers: {
       'Content-Type': 'application/json',
@@ -12,34 +12,28 @@ export async function createClient(data) {
 }
 
 export async function getClients() {
-  const token = localStorage.getItem("token");
-  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  const token = localStorage.getItem('token');
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
   return await axios.get(`/client`);
 }
 
 export async function getClient(id) {
-  const token = localStorage.getItem("token");
-  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  const token = localStorage.getItem('token');
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
   return await axios.get(`/client/${id}`);
 }
 
 export async function updateClient(data) {
-  const token = localStorage.getItem("token");
-  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-  console.log(data);
-  return await axios.patch(`/client/${data.id}`,
-    {
-      data
-    },
-    {
-      headers: {
-        "Content-Type": "application/json"
-      },
-    });
+  const token = localStorage.getItem('token');
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+  const returnData = await axios.put(`/client/${data.id}`, data);
+
+  console.log(returnData.data);
+  return returnData;
 }
 
 export async function deleteClient(id) {
-  const token = localStorage.getItem("token");
-  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+  const token = localStorage.getItem('token');
+  axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
   return await axios.delete(`/client/${id}`);
 }
